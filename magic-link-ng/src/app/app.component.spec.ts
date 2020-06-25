452075cd42193737239fa0eb05ed76cc7d0ebaa7
Daaren Urthling <daaren.urthling@hotmail.com>
2020-06-25 22:25:29 +0200

    renamed project

--- magic-link-ng/src/app/app.component.spec.ts
+++ magic-link-ng/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'magic-link'`, () => {
+  it(`should have as title 'magic-link-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('magic-link');
+    expect(app.title).toEqual('magic-link-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('magic-link app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('magic-link-ng app is running!');
   });
 });
